b50938815c29f8567690fe5a2d0a08fc752e51f5
SanhDNP <SanhDNP.CE190739@gmail.com>
2026-06-19 12:57:31 +0700

    Cart

--- frontend/src/features/Cart/screens/CartScreen.js
+++ frontend/src/features/Cart/screens/CartScreen.js
@@ -1,29 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useNavigate } from "react-router-dom";
 import CartItem from "../components/CartItem";
 import CartSummary from "../components/CartSummary";
 import { useCart } from "../../../store/CartContext";
-
+import { ROUTES } from "../../../constants/routes";
 export default function CartScreen() {
-  const {
-    items,
-    increaseQuantity,
-    decreaseQuantity,
-    removeItem,
-  } = useCart();
-
+  const { items, increaseQuantity, decreaseQuantity, removeItem } = useCart();
+  const navigate = useNavigate();
   return (
     <View style={styles.container}>
       <View style={styles.left}>
-        <Text style={styles.title}>
-          Giỏ hàng ({items.length})
-        </Text>
+        <Text style={styles.title}>Giỏ hàng ({items.length})</Text>
 
         {items.length === 0 ? (
-          <Text style={styles.emptyText}>
-            Giỏ hàng đang trống
-          </Text>
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyTitle}>Giỏ hàng trống</Text>
+
+            <Text style={styles.emptyDescription}>
+              Hãy thêm một số linh kiện tuyệt vời vào giỏ!
+            </Text>
+
+            <Pressable
+              style={styles.shopButton}
+              onPress={() => navigate(ROUTES.HOME)}
+            >
+              <Text style={styles.shopButtonText}>Khám phá sản phẩm</Text>
+            </Pressable>
+          </View>
         ) : (
           items.map((item) => (
             <CartItem
@@ -37,9 +41,11 @@ export default function CartScreen() {
         )}
       </View>
 
-      <View style={styles.right}>
-        <CartSummary />
-      </View>
+      {items.length > 0 && (
+        <View style={styles.right}>
+          <CartSummary />
+        </View>
+      )}
     </View>
   );
 }
@@ -72,5 +78,39 @@ const styles = StyleSheet.create({
   emptyText: {
     fontSize: 18,
     color: "#64748b",
+  },
+  emptyContainer: {
+    flex: 1,
+    minHeight: 500,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+
+  emptyTitle: {
+    fontSize: 36,
+    fontWeight: "800",
+    color: "#0f172a",
+    marginBottom: 12,
+  },
+
+  emptyDescription: {
+    fontSize: 18,
+    color: "#64748b",
+    marginBottom: 32,
+    textAlign: "center",
+  },
+
+  shopButton: {
+    backgroundColor: "#2563eb",
+    paddingHorizontal: 28,
+    paddingVertical: 14,
+    borderRadius: 12,
+  },
+
+  shopButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "700",
   },
 });
