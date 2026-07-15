bb01a2b65764a88661e1984bd92c0b5933281b99
SanhDNP <SanhDNP.CE190739@gmail.com>
2026-07-15 20:44:35 +0700

    Xóa File nhầm

--- frontend/frontend/src/constants/externalConstants.js
+++
@@ -1,5 +0,0 @@
-export const EXTERNAL_CONSTANTS = {
-  LEAFLET_CSS_URL: 'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.css',
-  LEAFLET_JS_URL: 'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.js',
-};
-
